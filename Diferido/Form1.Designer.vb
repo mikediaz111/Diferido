360eaa13c8da0256092ab865a58436df55c19f19
mikediaz111 <mike.diaz111@gmail.com>
2021-05-08 22:56:36 -0600

    commit

---
+++ Diferido/Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCompra
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtValor = New System.Windows.Forms.TextBox()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.txtInventario = New System.Windows.Forms.TextBox()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.txtIva = New System.Windows.Forms.TextBox()
+        Me.txtVenta = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Producto:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(94, 43)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(170, 23)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(52, 109)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Valor:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(45, 141)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 15)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Precio:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(25, 171)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(63, 15)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Inventario:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(30, 202)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 15)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Cantidad:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(61, 233)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(27, 15)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "IVA:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(49, 266)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 15)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Venta:"
+        '
+        'txtValor
+        '
+        Me.txtValor.Location = New System.Drawing.Point(94, 109)
+        Me.txtValor.Name = "txtValor"
+        Me.txtValor.Size = New System.Drawing.Size(170, 23)
+        Me.txtValor.TabIndex = 8
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(94, 141)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(170, 23)
+        Me.txtPrecio.TabIndex = 9
+        '
+        'txtInventario
+        '
+        Me.txtInventario.Location = New System.Drawing.Point(94, 171)
+        Me.txtInventario.Name = "txtInventario"
+        Me.txtInventario.Size = New System.Drawing.Size(170, 23)
+        Me.txtInventario.TabIndex = 10
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(94, 202)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(170, 23)
+        Me.txtCantidad.TabIndex = 11
+        '
+        'txtIva
+        '
+        Me.txtIva.Location = New System.Drawing.Point(94, 233)
+        Me.txtIva.Name = "txtIva"
+        Me.txtIva.Size = New System.Drawing.Size(170, 23)
+        Me.txtIva.TabIndex = 12
+        '
+        'txtVenta
+        '
+        Me.txtVenta.Location = New System.Drawing.Point(94, 266)
+        Me.txtVenta.Name = "txtVenta"
+        Me.txtVenta.Size = New System.Drawing.Size(170, 23)
+        Me.txtVenta.TabIndex = 13
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(270, 171)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 21)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "Nuevo"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(270, 266)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 23)
+        Me.Button2.TabIndex = 15
+        Me.Button2.Text = "Venta"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(394, 65)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(248, 199)
+        Me.ListBox1.TabIndex = 16
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(94, 80)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(170, 23)
+        Me.txtNombre.TabIndex = 17
+        '
+        'frmCompra
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(652, 338)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtVenta)
+        Me.Controls.Add(Me.txtIva)
+        Me.Controls.Add(Me.txtCantidad)
+        Me.Controls.Add(Me.txtInventario)
+        Me.Controls.Add(Me.txtPrecio)
+        Me.Controls.Add(Me.txtValor)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmCompra"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtValor As TextBox
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents txtInventario As TextBox
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents txtIva As TextBox
+    Friend WithEvents txtVenta As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents txtNombre As TextBox
+End Class
---
+++ Diferido/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class frmCompra
+    Dim productos As Integer
+    Dim cprod As New C_Productos
+    Private Sub frmCompra_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        productos = 0
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        cprod.llenarArray(productos, Me.txtNombre.Text, Me.txtValor.Text, Me.txtPrecio.Text, Me.txtInventario.Text)
+        productos = productos + 1
+
+        Me.ComboBox1.Items.Clear()
+
+        Dim cont As Integer
+        cont = 0
+        Do While cont < productos
+            Me.ComboBox1.Items.Add(cprod.obtenerDatos(cont, 1))
+            cont = cont + 1
+        Loop
+
+    End Sub
+
+    Private Sub txtCantidad_KeyDown(sender As Object, e As KeyEventArgs) Handles txtCantidad.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            Dim precio As Double
+            Dim row As Integer
+            Dim iva As Double
+            Dim cantidad As Double
+
+            cantidad = Convert.ToDouble(Me.txtCantidad.Text)
+            row = Me.ComboBox1.SelectedIndex
+            precio = cprod.obtenerDatos(row, 3) * cantidad
+            iva = cprod.calcularIva(precio)
+
+            Me.txtIva.Text = iva.ToString()
+            Me.txtVenta.Text = precio.ToString()
+        End If
+
+    End Sub
+End Class
+
+Public Class C_Productos
+    Dim codigo As Integer
+    Dim nombre As String
+    Dim valor As Double
+    Dim precioVenta As Double
+    Dim cantidadInventario As Integer
+    Dim arrProductos(5, 5) As String
+
+    Sub llenarArray(cant As Integer, nombre As String, valor As String, precio As String, inventario As String)
+        Dim codigo As Integer
+        Dim pos As Integer
+
+        codigo = cant + 1
+        pos = cant
+
+        arrProductos(pos, 0) = codigo.ToString()
+        arrProductos(pos, 1) = nombre.ToString()
+        arrProductos(pos, 2) = valor.ToString()
+        arrProductos(pos, 3) = precio.ToString()
+        arrProductos(pos, 4) = inventario.ToString()
+
+    End Sub
+
+    Function obtenerDatos(row As Integer, col As Integer)
+        Return arrProductos(row, col)
+    End Function
+
+
+
+    Function calcularIva(precio)
+        Dim iva As Double
+        iva = (precio * 0.13)
+
+        Return iva
+    End Function
+
+    Function venta(precio As Double, items As Integer)
+        Dim total As Double
+
+        total = precio * items
+
+        Return total
+    End Function
+End Class
